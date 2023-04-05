0fdaa6c2f1d9b587817a42ec02dc93a0b5113723
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-04-05 18:31:06 -0400

    codingChangleFinhsed

--- scrips/lectures.js
+++ scrips/lectures.js
@@ -1,5 +1,32 @@
 'use strict';
 /////////////////////////////////////////////////////////////////////////////////////////
+const pickup = {
+  make: 'Ford',
+  model: 'F150',
+  speed: 100
+}
+
+window.Car = function Car(props){
+  this.make = props.make;
+  this.model = props.model;
+  this.speed = props.speed;
+
+}
+Car.prototype.accelerate = function(param1){
+  this.speed += param1
+  l(this.speed)
+}
+
+const wolfsPickup = new Car(pickup);
+
+l(wolfsPickup.speed);
+wolfsPickup.accelerate(100);
+wolfsPickup.accelerate(1055);
+wolfsPickup.accelerate(-55);
+wolfsPickup.accelerate(-55);
+wolfsPickup.accelerate(-55);
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////
 const lectures = 'lectures';
--- scrips/script.js
+++ scrips/script.js
@@ -1,4 +1,29 @@
 'use strict';
+import { lectures } from './lectures.js';
 /////////////////////////////////////////////////////////////////////////////////////////
+const Corvett = {
+  make: 'Chevy',
+  model: 'Corvett',
+  speed: 200,
+  price: '100k'
+}
+
+const fastCar = function(props){
+  this.price =props.price;
+  this.race = function(){
+    this.speed += 100
+    l(this.speed)
+  }
+  Object.setPrototypeOf(this, new Car(props))
+
+}
+
+let kimsCar = new fastCar(Corvett)
+
+// l(kimsCar.speed);
+// kimsCar.accelerate();
+// kimsCar.race();
+// kimsCar.brake();
+// kimsCar.brake();
 
 /////////////////////////////////////////////////////////////////////////////////////////
