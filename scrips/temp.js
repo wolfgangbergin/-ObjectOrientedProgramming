5a7307ae932a1f9d5f07e096547cc031fadee565
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-04-06 09:39:50 -0400

    classes

--- scrips/temp.js
+++ scrips/temp.js
@@ -170,3 +170,58 @@ kim.test717();
 
  kim.payEmployee();
 /////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+const Corvett = {
+  make: 'Chevy',
+  model: 'Corvett',
+  speed: 200,
+  price: '100k'
+}
+
+const fastCar = function(props){
+  this.price =props.price;
+  this.race = function(){
+    this.speed += 100
+    l(this.speed)
+  }
+  Object.setPrototypeOf(this, new Car(props))
+
+}
+
+let kimsCar = new fastCar(Corvett)
+
+// l(kimsCar.speed);
+// kimsCar.accelerate();
+// kimsCar.race();
+// kimsCar.brake();
+// kimsCar.brake();
+/////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+const pickup = {
+  make: 'Ford',
+  model: 'F150',
+  speed: 100
+}
+
+window.Car = function Car(props){
+  this.make = props.make;
+  this.model = props.model;
+  this.speed = props.speed;
+
+}
+Car.prototype.accelerate = function(param1){
+  this.speed += param1
+  l(this.speed)
+}
+
+const wolfsPickup = new Car(pickup);
+
+l(wolfsPickup.speed);
+wolfsPickup.accelerate(100);
+wolfsPickup.accelerate(1055);
+wolfsPickup.accelerate(-55);
+wolfsPickup.accelerate(-55);
+wolfsPickup.accelerate(-55);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
