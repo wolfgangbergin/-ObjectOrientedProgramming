6e12ed3e53a4e2600e833c6c45635af8be36fda8
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-04-06 10:33:46 -0400

    classes

--- scrips/temp.js
+++ scrips/temp.js
@@ -225,3 +225,30 @@ wolfsPickup.accelerate(-55);
 
 
 /////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
+
+const PersonClass  = class  {
+  constructor(props){
+   
+   this.fName = props.fName;
+   this.bYear = props.bYear;
+   this.wolfTest = props.wolfTest
+  }
+ 
+  greet(){
+    l(`Hay im ${this.fName}`)
+  }
+}
+PersonClass.prototype.calcAge = function(){
+ l(`HI I'M ${this.fName} and I am ${2023 - +`${this.bYear}`} years old`);
+}; 
+
+let jojo = new PersonClass({ fName: 'jojo', bYear: '1899', wolfTest: 'test313' });
+
+
+l(jojo)
+// l(jojo.__proto__)
+// jojo.calcAge()
+//l(jojo.__proto__ === PersonClass.prototype)
+jojo.greet()
+/////////////////////////////////////////////////////////////////////////////////////////
