8681a4a7d905f8204aa07944e97f679cb05046d0
Wolfgang Bergin <wolfgangbergin@gmail.com>
2023-04-21 14:48:06 -0400

    codeingChallangefour

---
+++ scripts/carClass.js
@@ -0,0 +1,25 @@
+
+
+
+class Car {
+    constructor(props){
+        this.make = props.make,
+        this.speed = props.speed;
+    }
+}
+
+
+
+
+
+
+const wolfsCar = new Car({make: 'ford', speed: 0})
+
+
+
+
+
+
+
+export const static1 = 'static1'
+
--- scripts/ed/ed5.js
+++ scripts/ed/ed5.js
@@ -2,32 +2,34 @@
 
 class Account {
   //public
-  locale = navigator.language;
+  locale;
  
 
   //privit
   #movements = [];
   #pin;
-  static #privateStaticFieldWithInitializer = 'detroit';
+  static #city = 'detroit313';
   static #count = 0;
-
+ static #wolfTest;
   constructor(props) {
     (this.owner = props.owner),
       (this.currency = props.currency),
       (this.#pin = props.pin),
-      // this.locale = navigator.language;
+       this.locale = Account.#city;
       // this.#movements = [];
       (this.wolfTest = this.locale);
-   
+    Account.#wolfTest = props.owner
     //– Account.#AAAtest1('staticTest1')
   
   }
 
   deposit(val) {
     this.#movements.push(val);
+    return this
   }
   withdraw(val) {
     this.deposit(-val);
+    return this
   }
 
   requestLoan(val, param2) {
@@ -35,6 +37,7 @@ class Account {
 
     l(`your loan of ${val} was approved.`);
     this.deposit(val);
+    return this
   }
   static #AAAtest1(param1) {
     l(param1);
@@ -56,13 +59,13 @@ class Account {
 
     return true;
   }
-  static get City(){
-    return Account.#privateStaticFieldWithInitializer
+ get City(){
+    return Account.#city
   } 
 
-  static  set City(param1){
+   set City(param1){
 
-    Account.#privateStaticFieldWithInitializer = param1
+    Account.#city = param1
   }
 
   static get count3(){
@@ -74,6 +77,10 @@ class Account {
     Account.#count++
     return Account
   }
+  wolfLog(){
+    l(this.owner)
+    return this
+  }
 }
 
 
@@ -82,13 +89,15 @@ let wolfg = new Account({
   owner: 'wolfgang bergin',
   currency: 'USD',
   pin: 1234,
-  city: 'redford',
+  
 });
 
-Account.count2().count2().count2().count2().count2().count2().count2().count2()
-l(Account.count3)
+
+// wolfg.deposit(99).deposit(999).withdraw(100).requestLoan(1000, true)
 
 
+
+// l(wolfg.City)
 
 const script = ['script', 'scrips', `scripting`];
 export default script;
